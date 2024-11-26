99f67c659069f735fdde911675def66b521437c4
Minsung Kim <spongbob9876@naver.com>
2024-11-26 13:21:32 +0900

    Merge 3 charts

--- pokercraft_local/visualize.py
+++ pokercraft_local/visualize.py
@@ -1,6 +1,7 @@
 import math
 import typing
 
+import numpy as np
 import pandas as pd  # type: ignore [import-untyped]
 import plotly.express as px  # type: ignore [import-untyped]
 import plotly.graph_objects as plgo  # type: ignore [import-untyped]
@@ -26,12 +27,13 @@ BASE_HTML_FRAME: typing.Final[
 DEFAULT_WINDOW_SIZES: tuple[int, ...] = (50, 100, 200, 400, 800)
 
 
-def get_profit_line_chart(
+def get_historical_charts(
     tournaments: list[TournamentSummary],
     max_data_points: int = 2000,
+    window_sizes: tuple[int, ...] = DEFAULT_WINDOW_SIZES,
 ):
     """
-    Get profit line chart.
+    Get historical charts.
     """
     df_base = pd.DataFrame(
         {
@@ -39,107 +41,121 @@ def get_profit_line_chart(
             "Time": [t.start_time for t in tournaments],
             "Profit": [t.profit for t in tournaments],
             "Rake": [t.rake * t.my_entries for t in tournaments],
+            "Profitable": [1 if t.profit > 0 else 0 for t in tournaments],
+            "Buy In": [t.buy_in for t in tournaments],
         }
     )
     df_base["Net Profit"] = df_base["Profit"].cumsum()
     df_base["Net Rake"] = df_base["Rake"].cumsum()
     df_base["Ideal Profit w.o. Rake"] = df_base["Net Profit"] + df_base["Net Rake"]
+    df_base.index += 1
 
-    # Resampling
-    df_base = df_base.iloc[:: max(1, math.ceil(len(df_base) / max_data_points)), :]
-
-    return px.line(
-        df_base,
-        y=["Net Profit", "Net Rake", "Ideal Profit w.o. Rake"],
-        title="Net Profit & Rake Chart",
-        hover_data=["Tournament Name", "Time"],
-    )
-
-
-def get_recent_profitable_chart(
-    tournaments: list[TournamentSummary],
-    window_sizes: tuple[int, ...] = DEFAULT_WINDOW_SIZES,
-):
-    """
-    Get recent ITM ratio chart.
-    """
-    df_base = pd.DataFrame(
-        {
-            "Tournament Name": [t.name for t in tournaments],
-            "Time": [t.start_time for t in tournaments],
-            "Profitable": [1 if t.profit > 0 else 0 for t in tournaments],
-        }
-    )
+    # Profitable ratio
     profitable_expanding = df_base["Profitable"].expanding()
     df_base["Profitable Ratio"] = (
         profitable_expanding.sum() / profitable_expanding.count()
     )
-
-    minratio: float = 1.0
-    maxratio: float = 0.0
     for window_size in window_sizes:
         this_title = f"Profitable Ratio W{window_size}"
         df_base[this_title] = (
             df_base["Profitable"].rolling(window_size).sum() / window_size
         )
-        minratio = min(minratio, df_base[this_title].min())
-        maxratio = max(maxratio, df_base[this_title].max())
 
-    line = px.line(
-        df_base,
-        y=["Profitable Ratio"]
-        + [f"Profitable Ratio W{window_size}" for window_size in window_sizes],
-        title="Profitable Tournaments Ratio",
-        hover_data=["Tournament Name", "Time"],
-        range_y=[minratio - 0.02, maxratio + 0.02],
-    )
-    for window_size in window_sizes:
-        line.update_traces(
-            visible="legendonly",
-            selector={"name": f"Profitable Ratio W{window_size}"},
-        )
-    return line
-
-
-def get_recent_avg_buyin_chart(
-    tournaments: list[TournamentSummary],
-    window_sizes: tuple[int, ...] = DEFAULT_WINDOW_SIZES,
-):
-    """
-    Get recent average buy-in amount chart.
-    """
-    df_base = pd.DataFrame(
-        {
-            "Tournament Name": [t.name for t in tournaments],
-            "Time": [t.start_time for t in tournaments],
-            "Buy In": [t.buy_in for t in tournaments],
-        }
-    )
+    # Avg buy-in
     buyin_expanding = df_base["Buy In"].expanding()
     df_base["Avg Buy In"] = buyin_expanding.sum() / buyin_expanding.count()
-
-    min_buyin: float = math.inf
-    max_buyin: float = -math.inf
     for window_size in window_sizes:
         this_title = f"Avg Buy In W{window_size}"
         df_base[this_title] = df_base["Buy In"].rolling(window_size).mean()
-        min_buyin = min(min_buyin, df_base[this_title].min())
-        max_buyin = max(max_buyin, df_base[this_title].max())
 
-    line = px.line(
-        df_base,
-        y=["Avg Buy In"]
-        + [f"Avg Buy In W{window_size}" for window_size in window_sizes],
-        title="Average Buy In amount",
-        hover_data=["Buy In", "Tournament Name", "Time"],
-        log_y=True,
+    # Resampling
+    df_base = df_base.iloc[:: max(1, math.ceil(len(df_base) / max_data_points)), :]
+
+    figure = make_subplots(
+        rows=3,
+        cols=1,
+        shared_xaxes=True,
+        row_titles=["Net Profit & Rake", "Profitable Ratio", "Average Buy In"],
+        x_title="Tournament Count",
+        vertical_spacing=0.01,
     )
-    for window_size in window_sizes:
-        line.update_traces(
-            visible="legendonly",
-            selector={"name": f"Avg Buy In W{window_size}"},
+    common_options = {
+        "x": df_base.index,
+        "mode": "lines",
+        "customdata": np.stack((df_base["Tournament Name"],), axis=-1),
+    }
+
+    for col in ("Net Profit", "Net Rake", "Ideal Profit w.o. Rake"):
+        figure.add_trace(
+            plgo.Scatter(
+                y=df_base[col],
+                legendgroup="Profit",
+                legendgrouptitle_text="Profits & Rakes",
+                name=col,
+                hovertemplate="%{y:$,.2f}",
+                **common_options,
+            ),
+            row=1,
+            col=1,
         )
-    return line
+
+    for window_size in (0,) + window_sizes:
+        pr_col = (
+            "Profitable Ratio"
+            if window_size == 0
+            else f"Profitable Ratio W{window_size}"
+        )
+        name = "Since 0" if window_size == 0 else f"Recent {window_size}"
+
+        figure.add_trace(
+            plgo.Scatter(
+                y=df_base[pr_col],
+                meta=[y * 100 for y in df_base[pr_col]],
+                legendgroup="Profitable Ratio",
+                legendgrouptitle_text="Profitable Ratio",
+                name=name,
+                hovertemplate="%{meta:.3f}%",
+                **common_options,
+            ),
+            row=2,
+            col=1,
+        )
+        avb_col = "Avg Buy In" if window_size == 0 else f"Avg Buy In W{window_size}"
+        figure.add_trace(
+            plgo.Scatter(
+                y=df_base[avb_col],
+                legendgroup="Avg Buy In",
+                legendgrouptitle_text="Avg Buy In",
+                name=name,
+                hovertemplate="%{y:$,.2f}",
+                **common_options,
+            ),
+            row=3,
+            col=1,
+        )
+
+    figure.add_hline(
+        y=0,
+        line_color="red",
+        line_dash="dash",
+        row=1,
+        col=1,
+        label={
+            "text": "Break-even",
+            "textposition": "end",
+            "font": {"color": "red"},
+            "yanchor": "bottom",
+        },
+    )
+    figure.update_layout(
+        hovermode="x unified",
+        yaxis1={"tickformat": "$"},
+        yaxis2={"tickformat": "%"},
+        yaxis3={"tickformat": "$"},
+    )
+    figure.update_yaxes(row=3, col=1, patch={"type": "log"})
+    figure.update_traces(xaxis="x3")
+    return figure
 
 
 def get_profit_scatter_charts(tournaments: list[TournamentSummary]):
@@ -149,6 +165,7 @@ def get_profit_scatter_charts(tournaments: list[TournamentSummary]):
     non_freerolls = [t for t in tournaments if t.buy_in > 0]
     df_base = pd.DataFrame(
         {
+            "Tournament Name": [t.name for t in non_freerolls],
             "Buy In": [t.buy_in for t in non_freerolls],
             "Relative Prize": [t.my_prize / t.buy_in for t in non_freerolls],
             "Prize Ratio": [t.my_prize / t.total_prize_pool for t in non_freerolls],
@@ -172,18 +189,20 @@ def get_profit_scatter_charts(tournaments: list[TournamentSummary]):
             "By Total Entries (Nonzero profit only)",
         ],
         y_title="Relative Prize Return",
-        horizontal_spacing=0.05,
+        horizontal_spacing=0.01,
     )
     common_options = {
         "y": df_base["Relative Prize"],
         "mode": "markers",
         "marker_symbol": "circle",
+        "customdata": np.stack((df_base["Tournament Name"],), axis=-1),
     }
 
     figure1.add_trace(
         plgo.Scatter(
             x=df_base["Buy In"],
             name="RR by Buy In",
+            hovertemplate="RR=%{y:.2f}<br>BuyIn=%{x:.2f}<br>Name=%{customdata[0]}",
             **common_options,
         ),
         row=1,
@@ -193,6 +212,8 @@ def get_profit_scatter_charts(tournaments: list[TournamentSummary]):
         plgo.Scatter(
             x=df_base["Total Entries"],
             name="RR by Entries",
+            hovertemplate="RR=%{y:.2f}<br>TotalEntries=%{x:.2f}"
+            "<br>Name=%{customdata[0]}",
             **common_options,
         ),
         row=1,
@@ -224,20 +245,24 @@ def plot_total(
         lambda t: t.sorting_key()
     ),
     max_data_points: int = 2000,
+    window_sizes: tuple[int, ...] = DEFAULT_WINDOW_SIZES,
 ) -> str:
     """
     Plots the total prize pool of tournaments.
     """
     tournaments = sorted(tournaments, key=sort_key)
     figures = [
-        get_profit_line_chart(tournaments, max_data_points=max_data_points),
-        get_recent_profitable_chart(tournaments),
-        get_recent_avg_buyin_chart(tournaments),
+        get_historical_charts(
+            tournaments,
+            max_data_points=max_data_points,
+            window_sizes=window_sizes,
+        ),
         *get_profit_scatter_charts(tournaments),
     ]
     return BASE_HTML_FRAME % (
         nickname,
         "\n".join(
-            fig.to_html(include_plotlyjs="cdn", full_html=False) for fig in figures
+            fig.to_html(include_plotlyjs=("cdn" if i == 0 else False), full_html=False)
+            for i, fig in enumerate(figures)
         ),
     )
